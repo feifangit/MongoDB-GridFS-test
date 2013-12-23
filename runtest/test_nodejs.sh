1900cb835ac9abf2ed1d6e0a3cb293fcbf31bb64
neilchen <neilchen@futuredial.com>
2013-12-22 19:17:25 -0800

    modify test node.js ip

--- runtest/test_nodejs.sh
+++ runtest/test_nodejs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #node.js + mongodb driver
-ab -n 500 -c 5 http://127.0.0.1:3000/getfile/1k/ > node_1k.log
-ab -n 500 -c 5 http://127.0.0.1:3000/getfile/100k/ > node_100k.log
-ab -n 500 -c 5 http://127.0.0.1:3000/getfile/1m/ > node_1m.log
+ab -n 500 -c 5 http://10.31.219.246:3000/getfile/1k/ > node_1k.log
+ab -n 500 -c 5 http://10.31.219.246:3000/getfile/100k/ > node_100k.log
+ab -n 500 -c 5 http://10.31.219.246:3000/getfile/1m/ > node_1m.log
